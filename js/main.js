8f9600dbcd19beb80a9c36979ea1e00dc886b934
Dody Hartanto <muharridody@gmail.com>
2023-07-01 00:55:00 +0800

    add pic responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger-button');
+console.log(hamburgerButton);
+// const navMobile = document.getElementsByClassName('nav-mobile')[0];
+
+// hamburgerButton.addEventListener('click', function(){
+//     navMobile.classList.toggle('d-none')
+// }
+// );
